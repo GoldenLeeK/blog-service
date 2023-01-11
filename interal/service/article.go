0a49fcf75c8fb28f98f1c380b528ff57af7d99d8
lijh@95you.com <GoldenLeeK>
2023-01-11 15:59:34 +0800

    fix:1.修复创建帖子请求类的bug

--- interal/service/article.go
+++ interal/service/article.go
@@ -19,7 +19,7 @@ type CreateArticleRequest struct {
 	Title         string   `form:"title" binding:"max=128,required"`
 	Desc          string   `form:"desc" binding:"max=512,required"`
 	Content       string   `form:"content" binding:"required"`
-	CoverImageUrl string   `form:"desc" binding:"required"`
+	CoverImageUrl string   `form:"cover_image_url" binding:"required,url"`
 	CreatedBy     string   `form:"created_by" binding:"max=255,required"`
 	State         uint8    `form:"state,default=1" binding:"oneof=0 1"`
 	Tags          []uint32 `form:"tags" binding:"required"`
